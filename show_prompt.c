3828cdeb0ff5802eebaa729247b7fe8bd3b4b20f
raysummer9 <chimepromise433@gmail.com>
2023-12-13 23:30:58 +0100

    function that shows prompts

---
+++ show_prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * show_prompt - function that shows promit on the command line
+ * Return: return void
+ */
+
+void show_prompt(void)
+{
+	myprintf("simple_shell$ ");
+}
