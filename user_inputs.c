653dffd492452428b27be4692e4056bcd01c9155
raysummer9 <chimepromise433@gmail.com>
2023-12-13 23:38:27 +0100

    hot fix

--- user_inputs.c
+++ user_inputs.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * get_command - a function that gets command to the std output
+ * @cmd: command
+ * @size: size
+ * Return: returns void
+ */
+
 void get_command(char *cmd, size_t size)
 {
 	if (fgets(cmd, size, stdin) == NULL)
