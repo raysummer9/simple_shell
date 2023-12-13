30ae7b60ce1aa17032211eb7e931b2c51ddcf872
raysummer9 <chimepromise433@gmail.com>
2023-12-13 23:48:44 +0100

    hot fix

--- myprintf.c
+++ myprintf.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * myprintf - print function
+ * @str: string to print
+ * Return: void
+ */
+
 void myprintf(const char *str)
 {
 	write(STDOUT_FILENO, str, strlen(str));
